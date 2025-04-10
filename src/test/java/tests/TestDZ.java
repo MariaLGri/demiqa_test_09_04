7dcf1db3d41302fe58f15d6f1c5139c58dce5977
MLGrishina <maria107aa@gmail.com>
2025-04-10 22:12:51 +0300

    change build.gradle version adds SimpleJUnitTest.java, TestDZ.java

---
+++ src/test/java/tests/TestDZ.java
@@ -0,0 +1,31 @@
+package tests;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestDZ {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+    @Test
+    void fillFormTest() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Maria");
+        $("#userEmail").setValue("maria@gmail.com");
+        $("#currentAddress").setValue("Some street 1");
+        $("#permanentAddress").setValue("Another street 1");
+        $("#submit").click();
+
+        $("#output #name").shouldHave(text("Alex"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Some street 1"));
+        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+    }
+}
