127bd5e94301fe12bf4ab755c147f438b2a3b606
MLGrishina <maria107aa@gmail.com>
2025-04-10 23:33:42 +0300

    change TestDZ.java

--- src/test/java/tests/TestDZ.java
+++ src/test/java/tests/TestDZ.java
@@ -12,20 +12,25 @@ import static com.codeborne.selenide.Selenide.open;
 public class TestDZ {
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = "1920x1080"; // разрешение, ну это понятно
+        Configuration.pageLoadStrategy = "eager"; // чтоб не ждать загрузки всего сайта , картинок и т.д
+        Configuration.baseUrl = "https://demoqa.com"; // выносим абсолютный адресс из опен
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
     }
     @Test
     void fillFormTest() {
-        open("https://demoqa.com/text-box");
+        open("/text-box"); // первая форма логина
         $("#userName").setValue("Maria");
         $("#userEmail").setValue("maria@gmail.com");
         $("#currentAddress").setValue("Some street 1");
         $("#permanentAddress").setValue("Another street 1");
         $("#submit").click();
 
-        $("#output #name").shouldHave(text("Alex"));
-        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #name").shouldHave(text("Maria"));
+        $("#output #email").shouldHave(text("maria@gmail.com"));
         $("#output #currentAddress").shouldHave(text("Some street 1"));
         $("#output #permanentAddress").shouldHave(text("Another street 1"));
     }
+
 }
